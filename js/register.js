d867e893e0ccceb7c3445e0e617fdc089316a176
Kim YongHwan <ymin96@naver.com>
2019-11-07 16:48:36 +0900

    add modify

---
+++ js/register.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $("#uid").keyup(function (e) {
+        var uid = $(this).val();
+        $("#uid-group").removeClass("has-error");
+        $("#uid-group").removeClass("has-success");
+        if($("#uid").next().attr("id") === "help-uid"){
+            $("#uid").next().remove();
+        }
+
+        if (uid.length < 3) {
+            $("#uid-group").addClass("has-error");
+            $("#uid").after('<span id="help-uid" class="help-block">3글자 이상이어야합니다.</span>');
+        }
+        else {
+            var msg = {
+                userid : uid
+            }
+            $.ajax({
+                type: "POST",
+                url: "${contextPath}/register/uid",
+                contentType : "application/json; charset=UTF-8",
+                data: JSON.stringify(msg),
+                success: function (data) {
+                    console.log(data)
+                }
+            });
+        }
+    });
+});
