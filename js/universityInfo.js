16e79ce6a70b40621df99fee61b377d228675742
Yonghwan Kim <ymin96@naver.com>
2019-12-24 23:21:22 +0900

    add schedule insert and delete

--- js/universityInfo.js
+++ js/universityInfo.js
@@ -1,16 +1,13 @@
 
+var calendar;
+
 $(document).ready(function () {
     calendarEvent();
 });
 
 function calendarEvent(eventData) {
     var calendarEl = document.getElementById('calendar');
-    $("#calender").html("");
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-    var calendar = new FullCalendar.Calendar(calendarEl, {
+    calendar = new FullCalendar.Calendar(calendarEl, {
         plugins: ['interaction','dayGrid'],
         header: {
             left: "",
@@ -20,7 +17,44 @@ function calendarEvent(eventData) {
         },
         selectable: true,
         editable: true,
-        locale: 'kr'
+        locale: 'kr',
+        select:function(dateInfo){
+            $("#schedule-view").addClass("hidden");
+            $("#schedule-input").removeClass("hidden");
+            $("#startDate").val(dateInfo.startStr);
+            $("#endDate").val(dateInfo.endStr);
+        },
+        eventClick: function(info){
+            var dateInfo = info.event._def;
+            $("#schedule-input").addClass("hidden");
+            $("#schedule-view").removeClass("hidden");
+            $("#schedule-view .header span").html(dateInfo.title);
+            $("#schedule-view .contents p").html("· "+dateInfo.extendedProps.contents);
+            $("#view-delete").attr("info-id",dateInfo.publicId);
+        }
     });
     calendar.render();
+    var evt = {
+        id: 1,
+        allDay: true,
+        start: "2019-12-24",
+        end: "2019-12-25",
+        title: "테스팅",
+        contents: "내용"
+    }
+    calendar.addEvent(evt);
 }
+
+$("#input-cancel").click(function (e) { 
+    $("#schedule-input").addClass("hidden");
+});
+
+$("#view-submit").click(function (e) { 
+    $("#schedule-view").addClass("hidden");    
+});
+
+$("#view-delete").click(function (e) { 
+    var event = calendar.getEventById($(this).attr("info-id"));
+    event.remove();
+    $("#schedule-view").addClass("hidden");
+});
